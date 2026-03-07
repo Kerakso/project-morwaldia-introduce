e27c844bb3d5c7e766addd048ff5b6af34edf9da
Kerakso <oskarekpolak@gmail.com>
2026-03-08 00:01:33 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   basePath: '/project-morswaldia-introduce',
+  assetPrefix: '/project-morswaldia-introduce/',
   images: {
     unoptimized: true
-  }
+  },
+  reactStrictMode: true,
+  crossOrigin: 'anonymous',
+  trailingSlash: true,
 };
 
 export default nextConfig;
