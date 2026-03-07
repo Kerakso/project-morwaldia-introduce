ff6ded083562e4abbf05e963950355edf793d028
Kerakso <oskarekpolak@gmail.com>
2026-03-07 23:32:12 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/project-morswaldia-introduce',
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
